b501222fcd78f78e16655d0e81dd8dd4f0d522a5
Andrey Kurbanov <kurban1111@mail.ru>
2023-07-07 00:55:52 +0300

    Add comments

--- client.cpp
+++ client.cpp
@@ -24,19 +24,18 @@ int socket_descriptor;
 #endif
 struct sockaddr_in serveraddress;
 
+// Function to send a request to the server
 void sendRequest() {
-    // Укажем адрес сервера
+    // Set the server address
     serveraddress.sin_addr.s_addr = inet_addr("127.0.0.1");
-    // Зададим номер порта для соединения с сервером
     serveraddress.sin_port = htons(PORT);
-    // Используем IPv4
     serveraddress.sin_family = AF_INET;
 
-    // Создание сокета
+    // Create a socket
 #ifdef _WIN32
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cout << std::endl << "Failed to initialize winsock" << std::endl;
+        std::cerr << "Failed to initialize Winsock" << std::endl;
         return;
     }
     socket_descriptor = socket(AF_INET, SOCK_DGRAM, 0);
@@ -44,9 +43,9 @@ void sendRequest() {
     socket_descriptor = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 #endif
 
-    // Установка соединения с сервером
+    // Establish a connection with the server
     if (connect(socket_descriptor, (struct sockaddr*)&serveraddress, sizeof(serveraddress)) < 0) {
-        std::cout << std::endl << "Something went wrong. Connection Failed." << std::endl;
+        std::cerr << "Connection Failed" << std::endl;
 #ifdef _WIN32
         closesocket(socket_descriptor);
         WSACleanup();
@@ -56,10 +55,12 @@ void sendRequest() {
         return;
     }
 
-    while (1) {
-        std::cout << "Enter a message you want to send to the server: " << std::endl;
+    while (true) {
+        std::cout << "Enter a message to send to the server: " << std::endl;
         std::cin >> message;
+
         if (strcmp(message, "end") == 0) {
+            // Send the termination message to the server
             sendto(socket_descriptor, message, static_cast<int>(strlen(message)), 0, nullptr, sizeof(serveraddress));
             std::cout << "Client work is done!" << std::endl;
 #ifdef _WIN32
@@ -71,12 +72,14 @@ void sendRequest() {
             return;
         }
         else {
+            // Send the message to the server
             sendto(socket_descriptor, message, static_cast<int>(strlen(message)), 0, nullptr, sizeof(serveraddress));
             std::cout << "Message sent successfully to the server: " << message << std::endl;
-            std::cout << "Waiting for the Response from Server..." << std::endl;
+            std::cout << "Waiting for a response from the server..." << std::endl;
         }
 
-        std::cout << "Message Received From Server: " << std::endl;
+        std::cout << "Message received from server: " << std::endl;
+        // Receive the response from the server
         recvfrom(socket_descriptor, buffer, sizeof(buffer), 0, nullptr, nullptr);
         std::cout << buffer << std::endl;
     }
@@ -86,7 +89,7 @@ int main() {
 #ifdef _WIN32
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cout << "Failed to initialize winsock" << std::endl;
+        std::cerr << "Failed to initialize Winsock" << std::endl;
         return 1;
     }
 #endif
--- server.cpp
+++ server.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+
 #ifdef _WIN32
 #include <winsock2.h>
 #pragma comment(lib, "ws2_32.lib")
@@ -15,18 +16,21 @@
 char buffer[MESSAGE_BUFFER];
 char message[MESSAGE_BUFFER];
 int socket_file_descriptor;
+
 #ifdef _WIN32
 typedef int socklen_t;
 #endif
+
 socklen_t message_size, length;
 const char* end_string = "end";
 struct sockaddr_in serveraddress, client;
 
 void processRequest() {
+    // Initialize Winsock on Windows
 #ifdef _WIN32
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cerr << "Failed to initialize winsock" << std::endl;
+        std::cerr << "Failed to initialize Winsock" << std::endl;
         return;
     }
 #endif
@@ -45,7 +49,7 @@ void processRequest() {
     serveraddress.sin_port = htons(PORT);
     serveraddress.sin_family = AF_INET;
 
-    // Bind socket
+    // Bind socket to address and port
     if (bind(socket_file_descriptor, reinterpret_cast<struct sockaddr*>(&serveraddress),
         sizeof(serveraddress)) == -1) {
         std::cerr << "Failed to bind socket" << std::endl;
@@ -60,6 +64,8 @@ void processRequest() {
 
     while (true) {
         length = sizeof(client);
+
+        // Receive data from client
         message_size = recvfrom(socket_file_descriptor, buffer, sizeof(buffer) - 1, 0,
             reinterpret_cast<struct sockaddr*>(&client), &length);
         if (message_size == -1) {
@@ -73,8 +79,10 @@ void processRequest() {
             return;
         }
         buffer[message_size] = '\0';
+
+        // Check for termination signal
         if (strcmp(buffer, end_string) == 0) {
-            std::cout << "Server is Quitting" << std::endl;
+            std::cout << "Server is quitting" << std::endl;
 #ifdef _WIN32
             closesocket(socket_file_descriptor);
             WSACleanup();
@@ -83,17 +91,19 @@ void processRequest() {
 #endif
             return;
         }
+
         std::cout << "Message Received from Client: " << buffer << std::endl;
 
         // Reply to the client
         std::cout << "Enter reply message to the client: " << std::endl;
         std::cin >> message;
-        sendto(socket_file_descriptor, message, static_cast<size_t>(strlen(message)), 0,
+        sendto(socket_file_descriptor, message, strlen(message), 0,
             reinterpret_cast<struct sockaddr*>(&client), sizeof(client));
         std::cout << "Message Sent Successfully to the client: " << message << std::endl;
-        std::cout << "Waiting for the Reply from Client..!" << std::endl;
+        std::cout << "Waiting for the Reply from Client.." << std::endl;
     }
 
+    // Clean up resources
 #ifdef _WIN32
     closesocket(socket_file_descriptor);
     WSACleanup();
